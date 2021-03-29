3b67ce0f52f550535ba636a9d97b48e0fb9acf53
Vavyo <vavyo2121@gmail.com>
2021-03-29 18:08:46 -0500

    dog

---
+++ Zoo/Classes/Dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Classes
+{
+    public abstract class Dog: Mammal
+    {
+        // properties
+        public virtual string FurPattern => "solid";
+
+        // methods
+        public override string Attack()
+        {
+            return "Bite with my strong teeth";
+        }
+        public override string Cry()
+        {
+            return "Bark";
+        }
+        public override string Climb()
+        {
+            return "Unless it's a slope I'll have a hard time";
+        }
+    }
+}
