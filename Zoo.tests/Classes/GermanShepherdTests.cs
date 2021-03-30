0cf4a4e9df5456751991cefb378ee86a3675667c
Vavyo <vavyo2121@gmail.com>
2021-03-30 00:27:29 -0500

    German shepard tests

--- Zoo.tests/Classes/GermanShepherdTests.cs
+++ Zoo.tests/Classes/GermanShepherdTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Zoo.Classes;
 
 namespace Zoo.tests.Classes
 {
@@ -13,14 +14,27 @@ namespace Zoo.tests.Classes
         public void Age_is_set_in_constructor()
         {
             // arrange
+            GermanShepherd gus = new GermanShepherd("boy", 10);
+
             // act
+            int age = gus.Age;
+
             // assert
+            Assert.Equal(10, age);
+
         }
+        [Fact]
         public void Gender_is_set_in_constructor()
         {
             // arrange
+            GermanShepherd gus = new GermanShepherd("boy", 10);
+
             // act
+            string gender = gus.Gender;
+
             // assert
+            Assert.Equal("boy", gender);
+
         }
     }
 }
