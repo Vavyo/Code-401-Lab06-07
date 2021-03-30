44337f988dc664a84ffb97cf9273b065265b1416
Vavyo <vavyo2121@gmail.com>
2021-03-30 16:56:50 -0500

    IHasFur

---
+++ Zoo/Classes/IHasFur.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Classes
+{
+    public interface IHasFur
+    {
+        char FurLength { get; }
+
+        string FurPattern { get; }
+
+        string FurColor { get; }
+    }
+}
